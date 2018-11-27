c1edc96fed7b9dc23a83f15bee3a8d8715a4209b
dispe1 <lkjim0757@naver.com>
2018-11-27 12:11:48 +0900

    updage php code

--- server/image_upload_age.php
+++ server/image_upload_age.php
@@ -8,9 +8,10 @@ $size = $file["size"];
 $tmp_name = $file["tmp_name"];
 $temp = explode(".", $name);
 $extension = end($temp);
+$foldername = explode(".",explode("\\",$tmp_name)[3])[0];
 
 // uploads디렉토리에 파일을 업로드합니다.
-$uploaddir = './upload/image/';
+$uploaddir = '.\\'.$foldername.'\\image\\';
 $uploadfile = $uploaddir . basename($name);
 
 if(100000 < $size){
@@ -23,22 +24,24 @@ if(100000 < $size){
          if(!is_uploaded_file($tmp_name)) {
                echo "HTTP로 전송된 파일이 아닙니다.";
          } else {
+           if(!is_dir($foldername)){
+             mkdir($foldername);
+           }
+           if(!is_dir($foldername.'\\image')){
+             mkdir($foldername.'\\image');
+           }
                // move_uploaded_file은 임시 저장되어 있는 파일을 ./uploads 디렉토리로 이동합니다.
                if (move_uploaded_file($tmp_name, $uploadfile)) {
                     //echo "성공적으로 업로드 되었습니다.\n";
-                    $foldername = explode(".",explode("\\",$tmp_name)[3])[0];
-                    if(!is_dir($foldername)){
-                      mkdir($foldername);
-                    }
                     $python = 'C:\Users\lkjim\Anaconda3\envs\cuda\python.exe';
-                    $pyscript = 'C:\Apache24\htdocs\stargan\age_test.py --result_dir '.$foldername.'\\';
+                    $pyscript = 'C:\Apache24\htdocs\stargan\age_test.py --result_dir '.$foldername.'\\'.' --image_dir '.$foldername.'\\';
                     $cmd = "$python $pyscript";
                     exec($cmd);
-                    $imagedata = file_get_contents($foldername.'\\1-images.jpg');
-                    echo base64_encode($imagedata);
+                    echo base64_encode(file_get_contents($foldername.'\\1-images.jpg'));
                     //echo "<img src='stargan\\age\\results\\1-images.jpg' alt='result' />\n";
                     //echo "       original             5                  10                  15                 20                  25                   35                  45                 55                 65                  75\n";
                     unlink($uploadfile);
+                    rmdir($foldername.'\\image');
                     unlink($foldername.'\\1-images.jpg');
                     rmdir($foldername);
 
