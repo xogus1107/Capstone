4e3318f602ecf344eabc410fe441477d13035bf2
dispe1 <lkjim0757@naver.com>
2018-11-27 10:23:23 +0900

    update php code
    동일한 파일 명으로 결과 이미지를 생성하기 때문에
    다수의 사람이 이용했을 때 
    본인의 사진이 아닌 사진으로 생성된 사진을 결과로 받는 현상을 수정하였음

--- server/image_upload_emotion.php
+++ server/image_upload_emotion.php
@@ -25,15 +25,21 @@ if(100000 < $size){
                // move_uploaded_file은 임시 저장되어 있는 파일을 ./uploads 디렉토리로 이동합니다.
                if (move_uploaded_file($tmp_name, $uploadfile)) {
                     //echo "성공적으로 업로드 되었습니다.\n";
+                    $foldername = explode(".",explode("\\",$tmp_name)[3])[0];
+                    if(!is_dir($foldername)){
+                      mkdir($foldername);
+                    }
                     $python = 'C:\Users\lkjim\Anaconda3\envs\cuda\python.exe';
-                    $pyscript = 'C:\Apache24\htdocs\stargan\emotion_test.py';
+                    $pyscript = 'C:\Apache24\htdocs\stargan\emotion_test.py --result_dir '.$foldername.'\\';
                     $cmd = "$python $pyscript";
                     exec($cmd);
-                    $imagedata = file_get_contents('stargan\\age\\results\\1-images.jpg');
+                    $imagedata = file_get_contents($foldername.'\\1-images.jpg');
                     echo base64_encode($imagedata);
-                    //echo "<img src='stargan\\emotion\\results\\1-images.jpg' alt='result' />\n";
-                    //echo "      original             Anger              Contempt           Disgust               Fear            Happiness             Neutral             Sadness           Surprise\n";
+                    #echo "<img src=$foldername.'\\1-images.jpg' alt='result' />\n";
+                    #echo "      original             Anger              Contempt           Disgust               Fear            Happiness             Neutral             Sadness           Surprise\n";
                     unlink($uploadfile);
+                    unlink($foldername.'\\1-images.jpg');
+                    rmdir($foldername);
 
                } else {
                     echo "파일 업로드 실패입니다.\n";
